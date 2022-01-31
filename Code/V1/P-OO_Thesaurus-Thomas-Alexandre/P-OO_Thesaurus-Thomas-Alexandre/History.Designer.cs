3642e034911f1486f134af6f08217383a8138a89
Alexandresamuel King <pd50uyl@etmlnet.local>
2022-01-31 11:24:49 +0100

    première version, ajout visuel menu principal

---
+++ Code/V1/P-OO_Thesaurus-Thomas-Alexandre/P-OO_Thesaurus-Thomas-Alexandre/History.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace P_OO_Thesaurus_Thomas_Alexandre
+{
+    partial class History
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(History));
+            this.SuspendLayout();
+            // 
+            // History
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 611);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "History";
+            this.Text = "History";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Code/V1/P-OO_Thesaurus-Thomas-Alexandre/P-OO_Thesaurus-Thomas-Alexandre/Indexing.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace P_OO_Thesaurus_Thomas_Alexandre
+{
+    public partial class indexing : Form
+    {
+        DriveInfo[] allDrive = DriveInfo.GetDrives();
+        public indexing()
+        {
+            InitializeComponent();
+            foreach (DriveInfo drive in allDrive)
+            {
+                cmbBoxDisk.Items.Add(drive.Name);
+            }
+            cmbBoxDisk.SelectedIndex = 1;
+        }
+
+        private void cmbBoxDisk_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlusFilter_Click(object sender, EventArgs e)
+        {
+            cmbBoxResearch.Text += "+";
+        }
+
+        private void btnMinusFilter_Click(object sender, EventArgs e)
+        {
+            cmbBoxResearch.Text += "-";
+        }
+
+        private void btnANDFilter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnORFilter_Click(object sender, EventArgs e)
+        {
+            cmbBoxResearch.Text += "OR";
+        }
+    }
+}
